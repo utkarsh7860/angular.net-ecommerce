160dd1059c4a9659e9a26fa9b00ba240c86aebbd
Esha-Mangal <eshamangal18@gmail.com>
2022-03-22 15:43:54 +0530

    First Commit

---
+++ API/API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _repo;
+        public ProductsController(IProductRepository repo){
+            _repo = repo;
+            
+        }
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts(){
+            var products = await _repo.GetProductsAsync();
+            return Ok(products);
+            
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>>GetProduct(int id){
+            return await _repo.GetProductByIdAsync(id);
+        }
+    }
+}
---
+++ API/API/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {            
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope()){
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<LoggerFactory>();
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                }
+                catch(Exception ex){
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
+
+            }
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
